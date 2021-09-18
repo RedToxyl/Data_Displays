6c5ac981c97208ace92fdbd7270f00ad03179bb1
RedToxyl <65180349+RedToxyl@users.noreply.github.com>
2021-09-18 09:58:39 +0200

    test

---
+++ Display_Action/Macboot.py
@@ -0,0 +1,56 @@
+import time
+import Adafruit_SSD1306
+from PIL import Image
+
+
+whitescreen = Image.open("White.ppm").convert("1")
+su1 = Image.open("su1.ppm").convert("1")
+Apple = Image.open("Apple.ppm").convert("1")
+Welcome = Image.open("Welcome.ppm").convert("1")
+hello = Image.open("Hello2.pbm").convert("1")
+cat = Image.open('happycat_oled_64.ppm').convert('1')
+
+
+# Raspberry Pi pin configuration:
+RST = None
+
+# 128x32 display with hardware I2C:
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
+
+# Initialize library.
+disp.begin()
+
+# Clear display.
+while True:
+	disp.clear()
+	disp.display()
+
+	disp.image(su1)
+	disp.display()
+	time.sleep(3)
+
+	disp.image(whitescreen)
+	disp.display()
+	time.sleep(3)
+
+	disp.image(Apple)
+	disp.display()
+	time.sleep(3)
+
+	disp.image(Welcome)
+	disp.display()
+	time.sleep(5)
+
+	disp.image(hello)
+	disp.display()
+	time.sleep(10)
+
+	disp.image(cat)
+	disp.display()
+	time.sleep(10)
+
+	disp.clear()
+	disp.display()
+
+	if input() == "quit":
+		quit()
