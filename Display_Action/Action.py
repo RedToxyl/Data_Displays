dc6efa9a6e22292c69bee74ef8b9df6e8e2a215e
RedToxyl <65180349+RedToxyl@users.noreply.github.com>
2021-09-20 19:39:22 +0200

    first attempts at drawing text v1

---
+++ Display_Action/Action.py
@@ -0,0 +1,31 @@
+import time
+import Adafruit_SSD1306
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+# Definitions
+WIDTH = 128
+HEIGHT = 64
+PADDING = -2
+TOP = PADDING
+BOTTOM = HEIGHT - PADDING
+
+
+# 128x64 display with hardware I2C:
+disp = Adafruit_SSD1306.SSD1306_128_64()
+
+# startup
+disp.begin()
+
+disp.clear()
+disp.display()
+
+img = Image.new(1, (WIDTH, HEIGHT))
+draw = ImageDraw.Draw(img)
+
+font = ImageFont.load_default()
+
+while True:
+	draw.text((PADDING, PADDING), "9A - Englisch", 255, font)
+	draw.text((WIDTH / 2, HEIGHT / -2), "Grabow", 255, font)
