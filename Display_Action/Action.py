dd9b3f6c69f668361dbc7dbd8f27662c30380104
RedToxyl <65180349+RedToxyl@users.noreply.github.com>
2021-09-23 16:49:33 +0200

    adding room text v1

--- Display_Action/Action.py
+++ Display_Action/Action.py
@@ -33,11 +33,19 @@ def show_bloc(clss, subject, teacher, timebloc, room):
 	# drawing new text
 	draw.rectangle((0, 0, WIDTH, HEIGHT), outline=0, fill=0)  # clears display
 	draw.text((3, 3), f"{clss} - {subject}", 255, font)  # draws class and subject
+
 	# thank you very much, I can't be bothered with this: https://stackoverflow.com/questions/1970807/center-middle-align-text-with-pil
 	w, h = draw.textsize(teacher)
 	draw.text(((WIDTH - w) / 2, (HEIGHT - h) / 2 - 10), f"{teacher}", 255, font)  # draws teacher
-	draw.rectangle(((128 - int(HEIGHT / 3) - 15, 0), (128, int(HEIGHT / 3))), outline=255, fill=255)  # draws room square
-	#  draws room number
+
+	# draws room square
+	x1 = WIDTH - int(HEIGHT / 3) - 15
+	x2 = WIDTH
+	y1 = 0
+	y2 = int(HEIGHT / 3)
+	draw.rectangle(((x1, y1), (x2, y2)), outline=255, fill=255)
+	draw.text((x2 - (x2 - x1) / 2, (y2 - (y2 - y1) / 2)), text=f"{room}", fill=0, font=font, anchor="m")  # draws room number
+
 	w, h = draw.textsize(timebloc)
 	draw.text(((WIDTH - w) / 2, 40), f"{timebloc}", 255, )  # draws time
 
