41c0bcf7ff418063316be97a802210917c472aa5
RedToxyl <65180349+RedToxyl@users.noreply.github.com>
2021-09-23 16:04:47 +0200

    changing numbers v1

--- Display_Action/Action.py
+++ Display_Action/Action.py
@@ -31,7 +31,7 @@ def show_bloc(clss, subject, teacher, timebloc, room):
 	# drawing new text
 	draw.rectangle((0, 0, WIDTH, HEIGHT), outline=0, fill=0)  # clears display
 	draw.text((3, 3), f"{clss} - {subject}", 255, font)  # draws class and subject
-	draw.text((int(input()), int(input())), f"{teacher}", 255, font)  # draws teacher
+	draw.text((WIDTH / 2, HEIGHT / 2 - 5), f"{teacher}", 255, font)  # draws teacher
 	#  draws room square
 	#  draws room number
 	draw.text((int(input()), int(input())), f"{timebloc}", 255, font)  # draws time
