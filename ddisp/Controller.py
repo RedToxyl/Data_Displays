421216a1fd6702ca5d451b4e4bb45f91ceab5329
RedToxyl <65180349+RedToxyl@users.noreply.github.com>
2021-09-26 15:44:09 +0200

    very first rough draft of the controller

---
+++ ddisp/Controller.py
@@ -0,0 +1,32 @@
+import paho.mqtt.client as mqtt
+import time
+
+
+# get data
+# establish connection / logger
+
+# main loop
+	# handle connection errors
+
+	# stuffloop
+		# is new timebloc
+			# -> send new datablocs
+		# check responses?
+			# room has problem?
+	# check for keyboardinterrupt
+		# -> open menu
+
+
+# Definitions
+DATAPATH = "~/Users/thomaswolf/Desktop/Seminararbeit/Data_Displays/Helpfuls/ExamplePlan.json"
+BROKER = "192.168.178.45"
+CLIENTID = "ddisp_Controller"
+
+# TODO define Callbacks
+
+
+while True:
+	try:
+		pass
+	except ddispException:
+		pass
